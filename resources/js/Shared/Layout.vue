75ee0f25bd36dde866ff8eb66ef5f85393a8662b
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-10-29 03:12:43 +0000

    Shared Data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -11,7 +11,11 @@ export default {
 <template>
     <section class="p-6 bg-gray-200">
         <header class="container mx-auto flex items-center justify-between">
-            <h1 class="font-bold text-lg">My App</h1>
+            <div class="flex items-end divide-x divide-black">
+                <h1 class="font-bold text-lg pr-4">My App</h1>
+
+                <p class="text-sm pl-4">Welcome back, {{ $page.props.auth.user }}!</p>
+            </div>
 
             <Nav />
         </header>
